6420084579745ca27a3ee119dc6d0978f4bab57c
Nico Vece <me@nicovece.com>
2025-08-28 13:01:26 +0200

    added get book text and main functions

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+main()
+    
